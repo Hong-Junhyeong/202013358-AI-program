184ab465376f75520653986104f2588be549adc2
Hong-Junhyeong <liberonow2@gmail.com>
2026-03-25 16:51:00 +0900

    20260325

---
+++ loop0325_2.py
@@ -0,0 +1,10 @@
+'''
+score = [87, 84, 95, 67, 88, 94, 63]
+name = ["영호", "순자", "영식", "순희"],score]
+addressbook=[["영호", 25, "010-0000-0000"],["철수", 30, "010-0000-0000"]
+for i in addressbook
+    print(i)
+'''
+
+ri = list(range(5))
+print(ri)
