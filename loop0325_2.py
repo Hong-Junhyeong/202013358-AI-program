53dd670459400673f17f8a497fc87e402f170306
Hong-Junhyeong <liberonow2@gmail.com>
2026-03-25 17:44:15 +0900

    20260325

--- loop0325_2.py
+++ loop0325_2.py
@@ -6,5 +6,62 @@ for i in addressbook
     print(i)
 '''
 
+'''
 ri = list(range(5))
 print(ri)
+'''
+
+'''
+myString = 'apple'
+
+for ch in myString:
+    print(ch)
+
+listString = list(myString)
+print(listString)
+
+'''
+
+'''
+a_list = ['a', 'b', 'c', 'd', 'e']
+a_list.append('f')
+a_list
+'''
+
+'''
+a_list = ['a', 'b', 'c', 'd', 'e']
+a_list.remove('e')
+a_list
+'''
+
+
+'''
+n_list = [11,22,33,44,55,66]
+a = n_list.pop()
+
+print(a)
+print(n_list)
+'''
+
+'''
+a_list = [10, 20, 30, 40]
+
+10 in a_list
+50 in a_list
+10 not in a_list
+50 not in a_list
+'''
+
+'''
+list1 = [20, 10, 40, 50, 30]
+list2 = sorted(list1)
+print(list1)
+print(list2)
+'''
+
+list1 = [1, 2, 3, 4]
+list2 = [2, 3, 3, 4]
+list1 < list2
+
+
+
